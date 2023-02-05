ac865e6ae2e13d58c5a0fe94e77f588d9e3fb936
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-02-05 12:39:54 -0300

    🔧 Ajuste ordem da configuração antes da build

--- DeliveryApi/Program.cs
+++ DeliveryApi/Program.cs
@@ -40,6 +40,15 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer()
                 .AddSwaggerGen();
 
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 443;
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -49,14 +58,6 @@ app.UseSwagger();
 app.UseSwaggerUI();
 // }
 
-if (!builder.Environment.IsDevelopment())
-{
-    builder.Services.AddHttpsRedirection(options =>
-    {
-        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
-        options.HttpsPort = 443;
-    });
-}
 
 app.UseHttpsRedirection();
 
